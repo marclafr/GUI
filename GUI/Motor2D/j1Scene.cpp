9d2a8eec7a605788e6cc732d9285c0e2c485126c
marclafr <marc.lafrwy@gmail.com>
2017-06-20 19:19:55 +0200

    Base Code

---
+++ GUI/Motor2D/j1Scene.cpp
@@ -0,0 +1,84 @@
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Input.h"
+#include "j1Textures.h"
+#include "j1Render.h"
+#include "j1Window.h"
+#include "j1Gui.h"
+#include "j1Scene.h"
+
+j1Scene::j1Scene() : j1Module()
+{
+	name.assign("scene");
+}
+
+// Destructor
+j1Scene::~j1Scene()
+{}
+
+// Called before render is available
+bool j1Scene::Awake()
+{
+	LOG("Loading Scene");
+	bool ret = true;
+
+	return ret;
+}
+
+// Called before the first frame
+bool j1Scene::Start()
+{
+	// TODO 3: Create the image (rect {485, 829, 328, 103}) and the text "Hello World" as UI elements
+
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PreUpdate()
+{
+
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::Update(float dt)
+{
+	// Gui ---
+	
+	// -------
+	if(App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT)
+		App->render->camera.y += floor(200.0f * dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT)
+		App->render->camera.y -= floor(200.0f * dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT)
+		App->render->camera.x += floor(200.0f * dt);
+
+	if(App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT)
+		App->render->camera.x -= floor(200.0f * dt);
+
+	return true;
+}
+
+// Called each loop iteration
+bool j1Scene::PostUpdate()
+{
+	bool ret = true;
+
+	if(App->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
+		ret = false;
+
+	return ret;
+}
+
+// Called before quitting
+bool j1Scene::CleanUp()
+{
+	LOG("Freeing scene");
+
+	return true;
+}
